f766a0fddf4f3035aef37afae170a744b18bc61c
David Orlea <david.orlea@gmail.com>
2017-06-05 19:43:59 +0200

    Fix includes of MQTT subscription header

--- src/Smarty/MqttSubscription.hpp
+++ src/Smarty/MqttSubscription.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
+#include <cstdlib> // abort()
+#include <cstring> // strcmp()
 #include <functional>
 #include <vector>
-#include <Arduino.h>
 #include <Streaming.h>
 
 #define SMARTY_MQTT_SUBSCRIPTION_CALLBACK_TYPE std::function<void (const char* topic, const char* message)>
