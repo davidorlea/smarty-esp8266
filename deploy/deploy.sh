84697e00d6f28abc3e1cb10301c3b067a631507d
David Orlea <david.orlea@gmail.com>
2017-07-23 12:15:07 +0200

    Add deployment configuration for `Living Room Sofa Light`

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/KitchenSideboardLight.ini"
 pio ci examples/Custom/Custom.cpp --lib="." --project-conf="deploy/OutdoorFrontdoorLight.ini"
+pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/KitchenSideboardLight.ini"
+pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/LivingRoomSofaLight.ini"
