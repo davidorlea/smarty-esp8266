5745f5308a7a8a1afa0b24bf1e4579104325d8a8
David Orlea <david.orlea@gmail.com>
2017-07-28 11:57:49 +0200

    Add deployment configuration for `Bedroom Ambience Light` and remove configuration for development board

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,3 +3,4 @@
 pio ci examples/Custom/Custom.cpp --lib="." --project-conf="deploy/OutdoorFrontdoorLight.ini"
 pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/KitchenSideboardLight.ini"
 pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/LivingRoomSofaLight.ini"
+pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/BedroomAmbienceLight.ini"
