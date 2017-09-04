45011b9d046319080d092ef1e6306e8c9860485f
David Orlea <david.orlea@gmail.com>
2017-09-04 19:54:50 +0200

    Rename example of version 1 of custom board

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pio ci examples/Custom/Custom.cpp --lib="." --project-conf="deploy/OutdoorFrontdoorLight.ini"
+pio ci examples/CustomV1/CustomV1.cpp --lib="." --project-conf="deploy/OutdoorFrontdoorLight.ini"
 pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/KitchenSideboardLight.ini"
 pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/LivingRoomSofaLight.ini"
 pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/BedroomAmbienceLight.ini"
