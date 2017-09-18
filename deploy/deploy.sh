1accd9e772a1d3b90082d998f0f0c229cac549fb
David Orlea <david.orlea@gmail.com>
2017-09-18 18:12:21 +0200

    Update deployment script and definitions

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-pio ci examples/CustomV1/CustomV1.cpp --lib="." --project-conf="deploy/OutdoorFrontdoorLight.ini"
+pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/OutdoorFrontdoorLight.ini"
+pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/OutdoorBackdoorLight.ini"
+pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/KitchenCeilingLight.ini"
 pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/LivingRoomCeilingLight.ini"
-pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/KitchenSideboardLight.ini"
+pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/HallCeilingLight.ini"
 pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/LivingRoomSofaLight.ini"
 pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/BedroomAmbienceLight.ini"
+pio ci examples/SonoffBasic/SonoffBasic.cpp --lib="." --project-conf="deploy/KitchenSideboardLight.ini"
