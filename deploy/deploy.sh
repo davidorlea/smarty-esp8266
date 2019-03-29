7cfbf62693e1f7769d503fdac765539b9ba71536
David Orlea <david.orlea@gmail.com>
2019-03-29 13:56:10 +0100

    Fix typo in deployment script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,5 +6,5 @@ pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/KitchenCe
 pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/LivingRoomCeilingLight.ini"
 pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/HallCeilingLight.ini"
 pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/LivingRoomSofaLight.ini"
-pio ci examples/SonoffS20/SonoffS20.cpp--lib="." --project-conf="deploy/KitchenCabinetLight.ini"
+pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/KitchenCabinetLight.ini"
 pio ci examples/SonoffBasic/SonoffBasic.cpp --lib="." --project-conf="deploy/KitchenSideboardLight.ini"
