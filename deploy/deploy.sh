03aae6bc23ab0cb4a4d2d79fc03c04cf995444cd
David Orlea <david.orlea@gmail.com>
2019-05-06 15:12:29 +0200

    Add configuration for `Bedroom Ceiling Light`

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,6 +4,7 @@ pio ci examples/CustomV3/CustomV3.cpp --lib="." --project-conf="deploy/OutdoorFr
 pio ci examples/CustomV3/CustomV3.cpp --lib="." --project-conf="deploy/OutdoorBackdoorLight.ini"
 pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/KitchenCeilingLight.ini"
 pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/LivingRoomCeilingLight.ini"
+pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/BedroomCeilingLight.ini"
 pio ci examples/CustomV2/CustomV2.cpp --lib="." --project-conf="deploy/HallCeilingLight.ini"
 pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/LivingRoomSofaLight.ini"
 pio ci examples/SonoffS20/SonoffS20.cpp --lib="." --project-conf="deploy/KitchenCabinetLight.ini"
