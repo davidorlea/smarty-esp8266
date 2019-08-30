18437671979aa25b121dd349209254fbd83433f7
David Orlea <david.orlea@gmail.com>
2019-08-30 18:38:55 +0200

    Add Arduino version information to firmware JSON

--- src/Smarty/Firmware.cpp
+++ src/Smarty/Firmware.cpp
@@ -4,5 +4,9 @@ JsonObject& SmartyFirmware::toJson(JsonObject& rootJson) {
   JsonObject& firmwareJson = rootJson.createNestedObject("firmware");
   firmwareJson["version"] = version;
   firmwareJson["buildTime"] = buildTime;
+  JsonObject& arduinoJson = firmwareJson.createNestedObject("arduino");
+  arduinoJson["sdkVersion"] = ESP.getSdkVersion();
+  arduinoJson["coreVersion"] = ESP.getCoreVersion();
+  arduinoJson["fullVersion"] = ESP.getFullVersion();
   return firmwareJson;
 }
--- src/Smarty/Firmware.hpp
+++ src/Smarty/Firmware.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <Arduino.h>
 #include "AbstractJsonSerializable.hpp"
 
 class SmartyFirmware : public  SmartyAbstractJsonSerializable {
 public:
   using SmartyAbstractJsonSerializable::toJson;
-  static const unsigned int JSON_SIZE = JSON_OBJECT_SIZE(3) + 64;
+  static const unsigned int JSON_SIZE = JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(3) + 256;
   const char* version = nullptr;
   int buildTime = 0;
   JsonObject& toJson(JsonObject&) override;
